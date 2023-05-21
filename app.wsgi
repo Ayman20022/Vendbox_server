f9bd27f3ca0e06de3b85cea659acf045e3ffbe2e
ayman <elouazzani.aymane@inemail.ine.inpt.ma>
2023-05-21 19:21:23 +0100

    "some changes"

--- app.wsgi
+++ app.wsgi
@@ -4,3 +4,38 @@ sys.path.insert(0, './application.py')  # Replace '/path/to/your/flask/app' with
 from application import app  # Replace 'your_flask_app' with the name of your Flask app object
 
 application = app 
+
+
+
+
+
+
+
+I'm deploying my flask server on pythonanywhere.com , 
+in the Web page , Code area : 
+i have : 
+ - Source code : /home/elouazzaniaymane/mysite , under mysite 
+	i have two file , application.py which is the file for
+	my flask app , and key.json contains some info about 
+	firebase 
+ - in the wsgi configuration file , i have 
+	import sys
+
+	project_home = u'/home/elouazzaniaymane/mysite'
+	if project_home not in sys.path:
+    	sys.path = [project_home] + sys.path
+
+
+	from application import app as application
+
+once i launch the web app , i get this error : 
+Error running WSGI application
+2023-05-21 11:27:59,428: ModuleNotFoundError: No module named 'application'
+2023-05-21 11:27:59,428:   File "/var/www/elouazzaniaymane_pythonanywhere_com_wsgi.py", line 5, in <module>
+2023-05-21 11:27:59,429:     from application import app
+	
+
+
+
+
+
--- application.py
+++ application.py
@@ -1,39 +1,19 @@
-from flask import Flask, request,jsonify
-from bson.binary import Binary
-
-from pymongo import MongoClient 
-
-from flask_cors import CORS
-import json
 
 
-
-
-
-app = Flask(__name__)
-
-
+from flask import Flask,request,jsonify
 import firebase_admin
-from firebase_admin import credentials,firestore
+from firebase_admin import credentials
 import firebase_admin.db as fb_db
 
 
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://prodcut-f9355-default-rtdb.firebaseio.com/'})
 
-# import psycopg2
+app = Flask(__name__)
 
-# conn = psycopg2.connect(
-#     host="localhost",
-#     database="cart",
-#     user="dev",
-#     password="0000"
-# )
-
-# conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-
-
-# cur = conn.cursor()
-
-# create table if it doesn't exist
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
 
 def find_product_with_same_cart_product_id(pr_id,data):
         for e in data :
@@ -41,48 +21,31 @@ def find_product_with_same_cart_product_id(pr_id,data):
                 return e
         return None
 
-
-
 def list_without_null(list):
     list = [x for x in list if x!=None]
-    return list 
+    return list
 
 def get_correct_data(data):
-    if data != None : 
+    if data != None :
         if type(data) == type([]):
             return list_without_null(data)
-        else : 
+        else :
             return list_without_null(list(data.values()))
-    else : 
+    else :
         return []
 
-
-
-
-
 if __name__ == '__main__':
-    app.run(debug=False,host="0.0.0.0")
-
-
-
-
-
-
-cred = credentials.Certificate("key.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://prodcut-f9355-default-rtdb.firebaseio.com/'
-})
-
-
+    
+    app.run(debug=False, host="0.0.0.0")
 
 @app.route('/buy',methods=['POST'])
 def buy():
-    try : 
-        cart_data = get_correct_data(fb_db.reference('cart').get()) 
+    try :
+        cart_data = get_correct_data(fb_db.reference('cart').get())
         if len(cart_data):
             product_data = get_correct_data(fb_db.reference('products').get())
             updated = []
-            for cart_product in cart_data : 
+            for cart_product in cart_data :
                 print("cart_product : ",cart_product)
                 print("\n")
                 cart_product_id = cart_product['id']
@@ -92,7 +55,7 @@ def buy():
                 print("cart_product_flavor ",cart_product_flavor)
                 print("\n")
                 record_pieces = product_record['flavor'].get(cart_product_flavor)
-                if record_pieces>= cart_product_quantity : 
+                if record_pieces>= cart_product_quantity :
                     updated.append((
                         product_record['name'],True
                     ))
@@ -106,26 +69,27 @@ def buy():
                     print("new_product_record : ",product_record)
                     print("\n")
                     record_id = product_record['id']
-                    record_ref = fb_db.reference('products/' + str(record_id)) 
+                    record_ref = fb_db.reference('products/' + str(record_id))
                     print("product record that is going to be send to firebase : ",product_record)
                     print("\n")
                     record_ref.update(product_record)
-                    cart_data_=fb_db.reference('cart/'+str(record_id))
-                    cart_data_.delete()
-                else : 
+                else :
                     updated.append((
                         product_record['name'],False
                     ))
+            for product in cart_data:      
+                cart_data_=fb_db.reference('cart/'+str(product['id']))
+                cart_data_.delete()
             client_message = []
             for up in updated :
                 if up[1]:
                     client_message.append("succeful buy for : "+up[0])
-                else : 
+                else :
                     client_message.append("not enough pieces for : "+up[0])
             print(client_message)
-            return client_message
-        else : 
-            return "empty"
+            return jsonify(client_message)
+        else :
+            return jsonify("empty")
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -138,24 +102,25 @@ def buy():
 
 @app.route('/data', methods=['GET'])
 def get_data():
+    print('we are in')
     try:
         print('were in ')
         ref = fb_db.reference('cart')
         data = ref.get()
-        return get_correct_data(data)
+        return jsonify(get_correct_data(data))
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
+
 
 @app.route('/products', methods=['GET'])
 def get_products():
     try:
         ref = fb_db.reference('products')
         data = ref.get()
-        return get_correct_data(data)
+        return jsonify(get_correct_data(data))
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
+
 
 # define endpoint to add data to database
 @app.route('/data', methods=['POST'])
@@ -163,21 +128,10 @@ def add_data():
     try:
         cart_ref = fb_db.reference('cart')
         data = request.get_json()
-        id = data['id']
-        quantity = data['quantity']
-        flavor = data['flavor']
-        send = {
-            'id':id,
-            'quantity' : quantity,
-            'flavor':flavor
-        }
+        id=data['id']
         cart_ref.child(str(id)).set(data)
         return jsonify({'message': 'Data added successfully'})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-
-
-
 
 
